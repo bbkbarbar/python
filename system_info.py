0f70dd73cb55418ac53890e8b71aeddc9cf4f86e
Barbár <bbk.barbar@gmail.com>
2016-06-10 23:34:27 +0200

    Add system info py from example

--- system_info.py
+++ system_info.py
@@ -2,26 +2,26 @@ import subprocess
 import os
 
 def get_ram():
-	“Returns a tuple (total ram, available ram) in megabytes. See www.linuxatemyram.com”
+	"Returns a tuple (total ram, available ram) in megabytes. See www.linuxatemyram.com"
 	try:
-		s = subprocess.check_output([“free”,”-m”])
-		lines = s.split(‘\n’) 
+		s = subprocess.check_output(["free","-m"])
+		lines = s.split("\n") 
 		return ( int(lines[1].split()[1]), int(lines[2].split()[3]) )
 	except:
 		return 0
 
 def get_process_count():
-	“Returns the number of processes”
+	"Returns the number of processes"
 	try:
-		s = subprocess.check_output([“ps”,”-e”])
-		return len(s.split(‘\n’)) 
+		s = subprocess.check_output(["ps","-e"])
+		return len(s.split("\n")) 
 	except:
 	return 0
 
 def get_up_stats():
-	“Returns a tuple (uptime, 5 min load average)”
+	#"Returns a tuple (uptime, 5 min load average)"
 	try:
-		s = subprocess.check_output([“uptime”])
+		s = subprocess.check_output(["uptime"])
 		load_split = s.split(‘load average: ‘)
 		load_five = float(load_split[1].split(‘,’)[1])
 		up = load_split[0]
@@ -29,26 +29,26 @@ def get_up_stats():
 		up = up[:up_pos].split(‘up ‘)[1]
 		return ( up , load_five ) 
 	except:
-		return ( ” , 0 )
+		return ( "", 0 )
 
 def get_connections():
-	“Returns the number of network connections”
+	"Returns the number of network connections"
 	try:
-		s = subprocess.check_output([“netstat”,”-tun”])
+		s = subprocess.check_output(["netstat","-tun"])
 		return len([x for x in s.split() if x == ‘ESTABLISHED’])
 	except:
 		return 0
 
 def get_temperature():
-	“Returns the temperature in degrees C”
+	"Returns the temperature in degrees C"
 	try:
-		s = subprocess.check_output([“/opt/vc/bin/vcgencmd”,”measure_temp”])
+		s = subprocess.check_output(["/opt/vc/bin/vcgencmd","measure_temp"])
 		return float(s.split(‘=’)[1][:-3])
 	except:
 		return 0
 
 def get_ipaddress():
-	“Returns the current IP address”
+	"Returns the current IP address"
 	arg=’ip route list’
 	p=subprocess.Popen(arg,shell=True,stdout=subprocess.PIPE)
 	data = p.communicate()
@@ -57,7 +57,7 @@ def get_ipaddress():
 	return ipaddr
 
 def get_cpu_speed():
-	“Returns the current CPU speed”
+	"Returns the current CPU speed"
 	f = os.popen(‘/opt/vc/bin/vcgencmd get_config arm_freq’)
 	cpu = f.read()
 	return cpu
