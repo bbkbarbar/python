d92000387a289cd655bec325dc9d88e08442a78b
Barbár <bbk.barbar@gmail.com>
2016-06-11 17:28:52 +0200

    try to use colors

--- system_info.py
+++ system_info.py
@@ -111,17 +111,15 @@ def show_memory_info():
     
 #print("\033[1;32;40m Bright Green  \n")
 
-def printColor(text, color):
-	print("\033[" + color + text)
-
 def printColor(text, color, setNormal):
-	printColor(text, color)
+	print("\033[" + color + text)
 	if setNormal == 1:
 		printColor("","0;30;40m")
+		print("\033[0,30,40m")
 
 
 
-printColor("szoveg", "1;32;40m",1)
+printColor("szoveg","1;32;40m",1)
 
 print 'Temperature: ' +str(get_temperature()) + "'C"
 print 'CPU speed core: '+str(get_cpu_speed_current()) + " MHz"
