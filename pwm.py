5a254d0550408e22cb34a15cfb3a7e2c7d61b51f
Barbár <bbk.barbar@gmail.com>
2016-06-12 07:05:13 +0200

    PWM function first use

---
+++ pwm.py
@@ -0,0 +1,35 @@
+import RPi.GPIO as GPIO ## Import GPIO library
+import argparse
+
+GPIO.setwarnings(False)
+
+def msg(pin, state):
+	print "Pin: " + str(pin) + " State: " + str((value*100)/1024) + "%"
+
+def getBool(val):
+	if val == 0:
+		return False
+	else:
+		return True
+
+def main():
+	parser = argparse.ArgumentParser(' ')
+	parser.add_argument("-s","--state", type=int, help="Wanted state of output")
+	parser.add_argument("-p","--pin", type=int, help="Output pin", default = 3)
+
+	args = parser.parse_args()
+
+	pin = args.pin
+	value = args.state
+
+	GPIO.setmode(GPIO.BOARD) ## Use board pin numbering
+	msg(pin, value)
+
+	GPIO.setup(pin, GPIO.PWM) 
+
+	p = GPIO.PWM(pin, 0.5)
+	#p = GPIO.PWM(channel, frequency)
+	p.start(value)
+
+if __name__ == "__main__":
+	main()
