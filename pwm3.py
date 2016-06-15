8139ceb8646eff008e0f421e4febcdd495b5d02a
Barbár <bbk.barbar@gmail.com>
2016-06-15 17:25:28 +0200

    Argument based multiple channel pwm contol added

---
+++ pwm3.py
@@ -0,0 +1,44 @@
+import time
+import RPi.GPIO as GPIO
+
+def main():
+
+	parser = argparse.ArgumentParser('3ch PWM controller')
+	parser.add_argument("-c","--channel", type=int, help="output channel", default = 3)
+	parser.add_argument("-v","--value", type=int, help="pwm value (maybe in percentage)")
+	parser.add_argument("-f","--freq", type=int, help="pwm frequency", default = 200)
+
+	args = parser.parse_args()
+	channel = args.channel
+	value = args.value
+	freq = args.freq
+
+	if channel == 0:
+		chRed.stop()
+		GPIO.cleanup()
+		
+	else:
+		GPIO.setmode(GPIO.BOARD)
+
+		#pinRed = 3
+		#pinGreen = 5
+		#pinBlue = 7
+
+		GPIO.setup(channel, GPIO.OUT)
+		#GPIO.setup(pinRed, GPIO.OUT)
+		#GPIO.setup(pinGreen, GPIO.OUT)
+		#GPIO.setup(pinBlue, GPIO.OUT)
+
+		p = GPIO.PWM(channel, freq)  # channel=pinRed frequency=50Hz
+		#chGreen = GPIO.PWM(pinGreen, 200)  # channel=pinRed frequency=50Hz
+		#chBlue = GPIO.PWM(pinBlue, 200)  # channel=pinRed frequency=50Hz
+
+		p.start(0)
+		p.ChangeDutyCycle(value)
+
+		chRed.stop()
+		GPIO.cleanup()
+
+
+if __name__ == "__main__":
+	main()
