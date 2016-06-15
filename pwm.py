9601f184805606011479d70a4691ecf9964a6074
Barbár <bbk.barbar@gmail.com>
2016-06-15 20:08:25 +0200

    GPIO multichannel PWM testing

--- pwm.py
+++ pwm.py
@@ -5,8 +5,10 @@ GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(3, GPIO.OUT)
 
-p = GPIO.PWM(3, 0.5)
+#p = GPIO.PWM(3, 0.5)
+p = GPIO.PWM(3, 50)
+p.ChangeDutyCycle(90)
 p.start(1)
-input('Press return to stop:')   # use raw_input for Python 2
+raw_input('Press return to stop:')   # use raw_input for Python 2
 p.stop()
 GPIO.cleanup()
