d450098c2e1dd29ece37ef282082a25b3f455a21
Barbár <bbk.barbar@gmail.com>
2016-06-16 09:08:05 +0200

    Outputs removed (available in debug mode)

--- gpio/pwm_commander_client.py
+++ gpio/pwm_commander_client.py
@@ -2,6 +2,8 @@ import socket
 import sys
 import argparse
 
+debug_mode = False
+
 parser = argparse.ArgumentParser('PWM commander client')
 parser.add_argument("-c","--channel", type=str, help="output channel", default = 3)
 parser.add_argument("-v","--value", type=str, help="pwm value (maybe in percentage)")
@@ -12,14 +14,16 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Connect the socket to the port where the server is listening
 server_address = ('localhost', 7967)
-print >>sys.stderr, 'connecting to %s port %s' % server_address
+if debug_mode:
+    print >>sys.stderr, 'connecting to %s port %s' % server_address
 sock.connect(server_address)
 
 try:
     
     # Send data
     message = (args.value + " " + args.channel)
-    print >>sys.stderr, 'sending "%s"' % message
+    if debug_mode:
+        print >>sys.stderr, 'sending "%s"' % message
     sock.sendall(message)
 
     # Look for the response
@@ -29,7 +33,8 @@ try:
     while amount_received < amount_expected:
         data = sock.recv(32)
         amount_received += len(data)
-        print >>sys.stderr, 'received "%s"' % data
+        if debug_mode:
+            print >>sys.stderr, 'received "%s"' % data
 
 finally:
     #print >>sys.stderr, 'closing socket'
