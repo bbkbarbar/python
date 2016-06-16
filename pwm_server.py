395a316a49c9f560ada1e9cb9042b8fe58fb872e
Barbár <bbk.barbar@gmail.com>
2016-06-16 07:11:44 +0200

    basic pwm server created

--- pwm_server.py
+++ pwm_server.py
@@ -38,30 +38,30 @@ def setPwm(line):
 
 
 while serverIsRunning == True:
-    # Wait for a connection
-    print >>sys.stderr, 'waiting for a connection'
-    connection, client_address = sock.accept()
-    try:
-        print >>sys.stderr, 'connection from', client_address
+	# Wait for a connection
+	print >>sys.stderr, 'waiting for a connection'
+	connection, client_address = sock.accept()
+	try:
+		print >>sys.stderr, 'connection from', client_address
 
-        # Receive the data in small chunks and retransmit it
-        while True:
-            data = connection.recv(32)
-            #print >>sys.stderr, 'received "%s"' % data
-            if data:
-            	if data == "kill":
+		# Receive the data in small chunks and retransmit it
+		while True:
+			data = connection.recv(32)
+			#print >>sys.stderr, 'received "%s"' % data
+			if data:
+				if data == "kill":
 					p.stop()
 					GPIO.cleanup()
 					pwmIsRunning = False
 					serverIsRunning = False
 				else:
-                	#print >>sys.stderr, 'sending data back to the client'
-                	connection.sendall(data)
-                	setPwm(data)
-            else:
-                print >>sys.stderr, 'no more data from', client_address
-                break
-            
-    finally:
-        # Clean up the connection
-        connection.close()
+					#print >>sys.stderr, 'sending data back to the client'
+					connection.sendall(data)
+					setPwm(data)
+			else:
+				print >>sys.stderr, 'no more data from', client_address
+				break
+			
+	finally:
+		# Clean up the connection
+		connection.close()
