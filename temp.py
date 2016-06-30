0d55674ac8130c06f03e37efbe2b1213a1407231
Barbár <bbk.barbar@gmail.com>
2016-06-30 20:45:17 +0200

    Show temperature in Celsius only

--- temp.py
+++ temp.py
@@ -24,9 +24,9 @@ def read_temp():
 		temp_string = lines[1][equals_pos+2:]
 		temp_c = float(temp_string) / 1000.0
 		temp_f = temp_c * 9.0 / 5.0 + 32.0
-		return temp_c"C"
+		return temp_c
 		#, temp_f
 
 while True:
-	print(read_temp())
+	print(read_temp() + "C")
 	time.sleep(10)
