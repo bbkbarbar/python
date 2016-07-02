d332c68be64cc9b8e10c6595586fe6d0353cf9d6
Barbár <bbk.barbar@gmail.com>
2016-07-02 05:13:35 +0200

    Separated scripts for inline using added

---
+++ live/getTemp.py
@@ -0,0 +1,11 @@
+# Temp.py
+
+from ds18b20 import DS18B20   
+   
+# test temperature sensors
+x = DS18B20()
+count=x.device_count()
+i = 0
+while i < count:
+   print('%.3f' % x.tempC(i))
+   i += 1
