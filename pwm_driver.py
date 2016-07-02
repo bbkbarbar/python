ab142ce35e476cfc83a10d5f803b7d1f0d91f1f8
Barbár <bbk.barbar@gmail.com>
2016-07-02 07:34:29 +0200

    try to mod

--- pwm_driver.py
+++ pwm_driver.py
@@ -39,6 +39,6 @@ pwm.set_pwm_freq(60)
 
 print('Moving servo on channel 0, press Ctrl-C to quit...')
 
-for x in range(0, 2047)
+for x in range(0, 2047):
     pwm.set_pwm(0, 0, x)
     time.sleep(0.05)
