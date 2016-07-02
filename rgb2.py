b6acc88885ed3897157dc48620d143154edfe248
Barbár <bbk.barbar@gmail.com>
2016-07-02 12:50:46 +0200

    Can read rgb components from one string input

--- rgb2.py
+++ rgb2.py
@@ -36,11 +36,11 @@ allComponent = args.complete
 # Store values for each color-channel
 if allComponent[0] == "a":
 	red   = int(allComponent[1:4])  * 16
-	print "red:   " + red
+	print "red:   " + str(red)
 	green = int(allComponent[5:8])  * 16
-	print "green: " + green
+	print "green: " + str(green)
 	blue  = int(allComponent[8:10]) * 16
-	print "blue:  " + blue
+	print "blue:  " + str(blue)
 else:
 	red   = args.red
 	green = args.green
