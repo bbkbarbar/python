46402cd445227ca14e11b98aa9f931a2d0b10ba1
Barbár <bbk.barbar@gmail.com>
2016-07-02 11:03:44 +0200

    First test of pwm driver with rgb outputs

---
+++ rgb2.py
@@ -0,0 +1,56 @@
+# Simple demo of of the PCA9685 PWM servo/LED controller library.
+# This will move channel 0 from min to max position repeatedly.
+# Author: Tony DiCola
+# License: Public Domain
+from __future__ import division
+import time
+import argparse
+
+# Import the PCA9685 module.
+import Adafruit_PCA9685
+
+
+# Uncomment to enable debug output.
+#import logging
+#logging.basicConfig(level=logging.DEBUG)
+
+# Initialise the PCA9685 using the default address (0x40).
+pwm = Adafruit_PCA9685.PCA9685()
+
+# Alternatively specify a different address and/or bus:
+#pwm = Adafruit_PCA9685.PCA9685(address=0x41, busnum=2)
+
+# Configure min and max servo pulse lengths
+servo_min = 150  # Min pulse length out of 4096
+servo_max = 600  # Max pulse length out of 4096
+
+# Helper function to make setting a servo pulse width simpler.
+#-#def set_servo_pulse(channel, pulse):
+#-#    pulse_length = 1000000    # 1,000,000 us per second
+#-#    pulse_length //= 60       # 60 Hz
+#-#    print('{0}us per period'.format(pulse_length))
+#-#    pulse_length //= 4096     # 12 bits of resolution
+#-#    print('{0}us per bit'.format(pulse_length))
+#-#    pulse *= 1000
+#-#    pulse //= pulse_length
+#-#    pwm.set_pwm(channel, 0, pulse)
+
+# Set frequency to 60hz, good for servos.
+pwm.set_pwm_freq(600)
+
+parser = argparse.ArgumentParser('RGB test')
+parser.add_argument("-r","--red", type=int, help="red", default = 0)
+parser.add_argument("-g","--green", type=int, help="green", default = 0)
+parser.add_argument("-b","--blue", type=int, help="blue", default = 0)
+args = parser.parse_args()
+red   = args.red
+green = args.green
+blue  = args.blue
+
+print('Moving servo on channel 0, press Ctrl-C to quit...')
+
+#pwm.set_pwm(channel, 0, value)
+pwm.set_pwm(0, 0, red)
+pwm.set_pwm(1, 0, green)
+pwm.set_pwm(2, 0, blue)
+    
