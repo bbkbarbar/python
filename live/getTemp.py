7175f79437003ad10dc9e252c096d94d4d79ded0
bbkbarbar <bbk.barbar@gmail.com>
2016-07-04 13:17:59 +0200

    Logging added to 1-line runnable scripts

--- live/getTemp.py
+++ live/getTemp.py
@@ -1,7 +1,17 @@
 # Temp.py
 
 from ds18b20 import DS18B20   
+import time
 import argparse
+import os
+import datetime
+
+def write_to_file(data):
+	homePath = os.environ["HOME"]
+	with open(homePath + "/logs/read_temp.log", "a") as myfile:
+		myfile.write(data + "\r\n")
+		myfile.close()
+
 
 parser = argparse.ArgumentParser('Temperature reader')
 parser.add_argument("-s","--sensorChannel", type=int, help="temperature sensor channel", default = 0)
@@ -21,7 +31,12 @@ if sensor == -1:
 	   if i < count:
 	   		strResult += " "
 	print(strResult)
+	lasttime = str(datetime.datetime.time(datetime.datetime.now()))[:8]
+	write_to_file(lasttime + "\t" + strResult)
 else:
 	if sensor < count:
 		print('%.3f' % x.tempC(sensor))		
+		lasttime = str(datetime.datetime.time(datetime.datetime.now()))[:8]
+		write_to_file(lasttime + "\t" + '%.3f' % x.tempC(sensor))
+
 
